f6c82d2607b370afc10826be311c8c5dd06e03b4
VHM Infotecch <89240446+vhminfotech@users.noreply.github.com>
2022-07-01 17:24:03 +0530

    Revert "Merge remote-tracking branch 'origin/akanksha_more_page' into second_milestone"
    This reverts commit 6cc7ee4397d7511426860b1ece15725a6bd599f9, reversing
    changes made to 74e7b59633d927d13cbcc4428da22e755b068162.

--- lib/controllers/more/more_page_controller.dart
+++ lib/controllers/more/more_page_controller.dart
@@ -1,22 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MorePageController extends GetxController {
-  final List<String> moreItemsList = [
-    "Edit Profile",
-    "My Subscription",
-    "Change Password",
-    "Help Centre",
-    "Terms & Conditions",
-    "Privacy Policy",
-  ];
-  final List<String> iconItemsList = [
-    "assets/images/icon_edit_profile.png",
-    "assets/images/icon_my_subscription.png",
-    "assets/images/icon_change_password.png",
-    "assets/images/icon_help_center.png",
-    "assets/images/icon_info.png",
-    "assets/images/icon_info.png",
-  ];
 
   @override
   void onInit() {
@@ -29,7 +14,8 @@ class MorePageController extends GetxController {
   }
 
   @override
-  void onClose() {
+  void onClose(){
     super.onClose();
   }
+
 }
